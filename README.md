d57fa366cb8a1ce2ed9d235d2c1c21b141afcead
黄成都 <394042583@qq.com>
2015-09-28 12:41:20 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# SDWebImageStudy
-SDWebImage的源码分析和学习
+##学习博客地址列表：
+
+[SDWebImage源码分析--加载gif图片](http://www.bubuko.com/infodetail-633704.html)
+
+[SDWebImage源码解析之SDWebImageManager的注解](http://www.jianshu.com/p/6ae6f99b6c4c)
+
+
+[SDWebImage源码解析之SDWebImageManager的注解(2)](http://www.jianshu.com/p/0f9a7296f4c0)
+
--- SDWebImage/SDWebImageStudy/SDWebImage/SDWebImageManager.m
+++ SDWebImage/SDWebImageStudy/SDWebImage/SDWebImageManager.m
@@ -8,7 +8,9 @@
 
 #import "SDWebImageManager.h"
 #import <objc/message.h>
-
+/**
+ *  内部类
+ */
 @interface SDWebImageCombinedOperation : NSObject <SDWebImageOperation>
 
 @property (assign, nonatomic, getter = isCancelled) BOOL cancelled;
@@ -36,7 +38,18 @@
     });
     return instance;
 }
-
+/**
+ *  // 初始化方法.
+ 1.获得一个SDImageCache的单例.
+ 2.获取一个SDWebImageDownloader的单例.
+ 3.新建一个MutableSet来存储下载失败的url.
+ 4.新建一个用来存储下载operation的可变数组.
+ // 为什么不用MutableArray储存下载失败的URL?
+ // 因为NSSet类有一个特性,就是Hash.实际上NSSet是一个哈希表,哈希表比数组优秀的地方是什么呢?就是查找速度快.查找同样一个元素,哈希表只需要通过key
+ // 即可取到,而数组至少需要遍历依次.因为SDWebImage里有关失败URL的业务需求是,一个失败的URL只需要储存一次.这样的话Set自然比Array更合适.
+ *
+ *  @return
+ */
 - (id)init {
     if ((self = [super init])) {
         _imageCache = [self createCache];
@@ -46,11 +59,13 @@
     }
     return self;
 }
-
+// 获取一个cache的单例
 - (SDImageCache *)createCache {
     return [SDImageCache sharedImageCache];
 }
-
+// 利用Image的URL生成一个缓存时需要的key.
+// 这里有两种情况,第一种是如果检测到cacheKeyFilter不为空时,利用cacheKeyFilter来处理URL生成一个key.
+// 如果为空,那么直接返回URL的string内容,当做key.
 - (NSString *)cacheKeyForURL:(NSURL *)url {
     if (self.cacheKeyFilter) {
         return self.cacheKeyFilter(url);
@@ -59,18 +74,23 @@
         return [url absoluteString];
     }
 }
-
+// 检测一张图片是否已被缓存.
+// 首先检测内存缓存是否存在这张图片,如果已有,直接返回yes.
+// 如果内存缓存里没有这张图片,那么调用diskImageExistsWithKey这个方法去硬盘缓存里找
 - (BOOL)cachedImageExistsForURL:(NSURL *)url {
     NSString *key = [self cacheKeyForURL:url];
     if ([self.imageCache imageFromMemoryCacheForKey:key] != nil) return YES;
     return [self.imageCache diskImageExistsWithKey:key];
 }
-
+// 检测硬盘里是否缓存了图片
 - (BOOL)diskImageExistsForURL:(NSURL *)url {
     NSString *key = [self cacheKeyForURL:url];
     return [self.imageCache diskImageExistsWithKey:key];
 }
-
+// 首先生成一个用来cache 住Image的key(利用key的url生成)
+// 然后检测内存缓存里是否已经有这张图片
+// 如果已经被缓存,那么再主线程里回调block
+// 如果没有检测到,那么调用diskImageExistsWithKey,这个方法会在异步线程里,将图片存到硬盘,当然在存图之前也会检测是否已在硬盘缓存图片.
 - (void)cachedImageExistsForURL:(NSURL *)url
                      completion:(SDWebImageCheckCacheCompletionBlock)completionBlock {
     NSString *key = [self cacheKeyForURL:url];
@@ -94,7 +114,7 @@
         }
     }];
 }
-
+//将图片存入硬盘
 - (void)diskImageExistsForURL:(NSURL *)url
                    completion:(SDWebImageCheckCacheCompletionBlock)completionBlock {
     NSString *key = [self cacheKeyForURL:url];
@@ -106,7 +126,7 @@
         }
     }];
 }
-
+// 通过url建立一个operation用来下载图片.
 - (id <SDWebImageOperation>)downloadImageWithURL:(NSURL *)url
                                          options:(SDWebImageOptions)options
                                         progress:(SDWebImageDownloaderProgressBlock)progressBlock
@@ -127,12 +147,15 @@
 
     __block SDWebImageCombinedOperation *operation = [SDWebImageCombinedOperation new];
     __weak SDWebImageCombinedOperation *weakOperation = operation;
-
+    // 创建一个互斥锁防止现在有别的线程修改failedURLs.
+    // 判断这个url是否是fail过的.如果url failed过的那么isFailedUrl就是true
     BOOL isFailedUrl = NO;
     @synchronized (self.failedURLs) {
         isFailedUrl = [self.failedURLs containsObject:url];
     }
-
+    // 如果url不存在那么直接返回一个block,如果url存在.那么继续进行判断.
+    // options与SDWebImageRetryFailed这个option进行按位与操作.判断用户的options里是否有retry这个option.
+    // 如果用户的options里没有retry这个选项并且isFaileUrl 是true.那么就回调一个error的block.
     if (!url || (!(options & SDWebImageRetryFailed) && isFailedUrl)) {
         dispatch_main_sync_safe(^{
             NSError *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorFileDoesNotExist userInfo:nil];
@@ -140,7 +163,7 @@
         });
         return operation;
     }
-      
+    // 创建一个互斥锁防止现在有别的线程修改runningOperations.
     @synchronized (self.runningOperations) {
         [self.runningOperations addObject:operation];
     }
