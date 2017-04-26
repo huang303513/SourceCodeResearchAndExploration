05aa272a1602496fec1071bf62fa9e0dd7a0fe72
huangchengdu <394042583@qq.com>
2017-04-26 21:38:29 +0800

    update

---
+++ SDWebImage4.X源码阅读201704/SDWebImage4.X源码阅读201704/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDWebImage4.X源码阅读201704
+//
+//  Created by huangchengdu on 17/4/26.
+//  Copyright © 2017年 huangchengdu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDWebImage4.X源码阅读201704/SDWebImage4.X源码阅读201704/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDWebImage4.X源码阅读201704
+//
+//  Created by huangchengdu on 17/4/26.
+//  Copyright © 2017年 huangchengdu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
