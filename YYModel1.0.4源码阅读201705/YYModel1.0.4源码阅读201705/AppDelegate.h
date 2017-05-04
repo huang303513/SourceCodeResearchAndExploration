4b1f0933c6e4c2b8e2778f36eb1f31dd1641e747
huangchengdu <394042583@qq.com>
2017-05-04 21:14:15 +0800

    YYModel基本使用

---
+++ YYModel1.0.4源码阅读201705/YYModel1.0.4源码阅读201705/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYModel1.0.4源码阅读201705
+//
+//  Created by huangchengdu on 17/5/3.
+//  Copyright © 2017年 huangchengdu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYModel1.0.4源码阅读201705/YYModel1.0.4源码阅读201705/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  YYModel1.0.4源码阅读201705
+//
+//  Created by huangchengdu on 17/5/4.
+//  Copyright © 2017年 huangchengdu. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
